54c0ad4ae647c2a8cd89694c431d6c8f0759a04e
raviranve <raviranve786@gmail.com>
2025-05-28 01:17:39 +0530

    add vercel json file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//     react()
+//   ],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+  base: '/', // Important for Vercel routing
+  plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
